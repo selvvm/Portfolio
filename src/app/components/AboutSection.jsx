1209646071ce84fde55ebcfd8d008445b8782531
Mariselvamkannnancorob <Mariselvam.Kannan@corob.com>
2024-08-25 15:58:06 +0530

    commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>C, C++, Python, Java, JavaScript, SQL</li>
+        <li>HTML, CSS, React, React Native</li>
+        <li>QT, QML, Spring Boot, Django</li>
+        <li>Android Studio, Git, VS Code</li>
+        <li>Docker, Kubernetes</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Vidyalankar Institute Of Technology, Mumbai University</li>
+        <li>Bachelor of Engineering in Electronics</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>COROB - Software Engineer, Research and Development Team</li>
+        <li>Telit Cinterion - Software Engineer, Application Engineer Team</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am an experienced Software Engineer with 2 years of dedicated work in the Research and Development sector. 
+            I have a strong background in developing user-friendly software applications using various technologies including 
+            QT, QML, C++, and Java for Android and Windows platforms. I'm passionate about collaborating within development 
+            teams to ideate and implement innovative features, enhancing user experience. My experience extends to product 
+            management roles, leading PRD discussions and tech enhancement backlogs. I also have experience in client 
+            communications with renowned clients, understanding their problems and pitching solutions.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              {" "}
+              Experience{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,142 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "BugFlow",
+    description:
+      "A modern issue tracking application built with Next.js, NextAuth, Prisma, Tailwind CSS, and MySQL.",
+    image: "/images/projects/issuetracker.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/selvvm/BugFlow",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Sports Live",
+    description:
+      "Real-time sports updates application using Spring Boot, Angular, and Socket.IO",
+    image: "/images/projects/sportslive.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/selvvm/Sports-info-live",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Blog-Go",
+    description: "AI-powered blog application using Java, Spring Boot, and React",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/selvvm/Blog-Go-An-Ai-powered-web-application",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Custom Heap Allocator",
+    description: "Custom heap allocator with garbage collector implemented in C",
+    image: "/images/projects/5.png",
+    tag: ["All", "Systems"],
+    gitUrl: "https://github.com/selvvm/Custom-Heap-Allocator-with-Garbage-Collector-",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "TCP Chat Server in Go",
+    description:
+      "A simple TCP chat server implemented in Go that allows multiple clients to connect and communicate in real-time.",
+    image: "/images/projects/5.png", // You'll need to add this image
+    tag: ["All", "Systems"],
+    gitUrl: "https://github.com/selvvm/Mutichat-Like-YT-or-insta-live-chat",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Simple Neural Network for XOR Problem",
+    description:
+      "A C++ implementation of a simple neural network designed to solve the XOR problem using a feedforward architecture with backpropagation.",
+    image: "/images/projects/5.png", // You'll need to add this image
+    tag: ["All", "Systems"],
+    gitUrl: "https://github.com/selvvm/Training-Single-Neuron-in-C-",
+    previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "ShopinglyX",
+    description:
+      "An e-commerce website built with Django, featuring a payment gateway integration.",
+    image: "/images/projects/3.png", // You'll need to add this image
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/selvvm/ShopinglyX",
+    previewUrl: "/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Systems"
+          isSelected={tag === "Systems"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
